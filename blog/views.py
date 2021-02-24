6a3b7c144caa7eec442c81158075f0a0c8c79a6b
wark1308 <dablkill3277@gmail.com>
2021-02-24 20:19:13 +0600

    4

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
-
+from .models import Post, PostImage
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def posts_list(requests):
+def posts_list(requests, pk):
     return HttpResponse('Hello')
 
+def new_posts(request, pk):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'new_posts.html', locals())
 
+def edit_post(request, pk):
+    pass
+
+def post_delete(request, pk):
+    pass
