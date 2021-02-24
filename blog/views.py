7e0bdcb036c77e413087f44cc933d06ea41b74a0
wark1308 <dablkill3277@gmail.com>
2021-02-24 19:46:33 +0600

    second

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django import request
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
