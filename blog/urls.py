e363e313bcfa820fdc7e33d92244c6fc54d268ad
erbol123 <erbolabdykalylov@gmail.com>
2021-02-24 20:12:48 +0600

    Erbol

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.urls import path
-from .views import hh
 
-urlpatterns = [
-    path('', hh, name='hh')
-]
-=======
 from django.urls import path
 
 from .views import *
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,3 @@ from django.http import HttpResponse
 
 def posts_list(requests):
     return HttpResponse('Hello')
-
-
-<<<<<<< HEAD
-def hh(request):
-    return render(request, 'base.html')
-=======
->>>>>>> a68f00abb5cd51d2bffbaa8bdd4171ff66bf7bcd
