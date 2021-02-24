21756107cadcb2472271bdb83e7ebb242c0ae833
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-02-24 20:15:54 +0600

    Just commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
 
 def posts_list(requests):
-    return HttpResponse('Hello')
+    posts = Post.objects.all()
+    context = {
+        'posts':posts
+    }
+    return render(request, 'posts_list.html', context)
 
 
