69b7e8a4575ab612535a490d7f753cd8556e5e05
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-02-28 21:53:43 +0600

    Aktan10

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('home/', posts_list, name='posts_list'),
     path('detail_post/<int:pk>/', post_detail, name='post_detail'),
+    path('my/profile/<str:username>/', my_profile, name='my_profile'),
     path('new_twit/', new_twit, name='new_twit'),
     path('delete_twit/<int:pk>/', delete_twit, name='delete_twit'),
     path('edit/twit/<int:pk>/', edit_twit, name='edit_twit'),
